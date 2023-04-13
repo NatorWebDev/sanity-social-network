1af21172d4f26458ea1c67d44630b75d0ab2e5cf
NatorWebDev <126914912+NatorWebDev@users.noreply.github.com>
2023-04-13 18:51:11 -0400

    added spinner component, working on feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,4 +1,15 @@
+import {useState,useEffect} from 'react'
+import {useParams} from 'react-router-dom'
+
+import { client} from "../client";
+import MasonryLayout from "./MasonryLayout";
+import Spinner from "./Spinner";
+
 export default function Feed() {
+  const [loading, setLoading] = useState(true)
+
+  if(lodaing) return <Spinner message="we are adding new ideas to your feed!"/>
+
   return (
     <div>Feed</div>
   )
---
+++ src/components/MasonryLayout.jsx
@@ -0,0 +1,5 @@
+export const MasonryLayout = () => {
+  return (
+    <div>MasonryLayout</div>
+  )
+}
