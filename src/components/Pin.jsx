0f8f35bd23517d64f9fd0beccdb281e9248c97a1
NatorWebDev <126914912+NatorWebDev@users.noreply.github.com>
2023-04-14 19:01:54 -0400

    created the masonry for pins and the lading for those pins + rendering their imagage

--- src/components/Pin.jsx
+++ src/components/Pin.jsx
@@ -1,10 +1,53 @@
-import { urlFor } from "../client";
+import { useState } from "react";
+import { urlFor, client } from "../client";
+import { Link, useNavigate } from "react-router-dom";
+import { v4 as uuidv4 } from "uuid";
 
+import { MdDownloadForOffline } from "react-icons/md";
+import { AiTwotoneDelete } from "react-icons/ai";
+import { BsFillArrowUpRightCircleFill } from "react-icons/bs";
 
-export default function Pin({pin:{postedBy,image,_id,destination}}) {
+export default function Pin({ pin: { postedBy, image, _id, destination } }) {
+  const [postHovered, setPostHovered] = useState(false);
+  const [savingPost, setSavingPost] = useState(false);
+  const navigate = useNavigate();
+
   return (
-    <div>
-        <img src={urlFor(image).width(250).url()} alt="user-post" className="rounded-lg w-full" />
+    <div className="m-2">
+      <div
+        onMouseEnter={() => setPostHovered(true)}
+        onMouseLeave={() => setPostHovered(false)}
+        onClick={() => navigate(`./pin-detail/${_id}`)}
+        className="relative cursor-zoom-in w-auto hover:shadow-lg rounded-lg overflow-hidden transition-all duration-500 ease-in-out"
+      >
+        <img
+          src={urlFor(image).width(250).url()}
+          alt="user-post"
+          className="rounded-lg w-full"
+        />
+        {postHovered && (
+          <div
+            className="absolute top-0 w-full h-full flex flex-col justify-between p-1 pr-2 pt-2 pb-2 z-50"
+            /* style={{ height: "100%" }} */
+          >
+            <div className="flex items-center justify-between">
+              <div className="flex gap-2">
+                <a
+                  href={`${image?.asset?.url}?dl=`}
+                  download
+                  onClick={(e) => {
+                    e.stopPropagation();
+                  }}
+                >
+                    <MdDownloadForOffline className="bg-white w-9 h-9 rounded-full flex items-center justify-center text-dark text-xl opacity-75 hover:opacity-100 hover:shadow-md outline-none"/>
+                </a>
+              </div>
+              awa
+            </div>
+            uwu
+          </div>
+        )}
+      </div>
     </div>
-  )
+  );
 }
