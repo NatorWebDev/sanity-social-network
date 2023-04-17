b0de5149357fcf38f5f170e31ca7d77f237651f5
NatorWebDev <126914912+NatorWebDev@users.noreply.github.com>
2023-04-17 19:02:09 -0400

    added detail pin queries and start working on pin-detail component

--- src/components/PinDetail.jsx
+++ src/components/PinDetail.jsx
@@ -1,5 +1,46 @@
-export default function PinDetail() {
-  return (
-    <div>PinDetail</div>
-  )
+import { useState, useEffect } from "react";
+import { MdDownloadForOffline } from "react-icons/md";
+import { Link, useParams } from "react-router-dom";
+import { v4 as uuidv4 } from "uuid";
+
+import { client, urlFor } from "../client";
+import MasonryLayout from "./MasonryLayout";
+import { pinDetailMorePinQuery, pinDetailQuery } from "../utils/data";
+import Spinner from "./Spinner";
+
+export default function PinDetail({ user }) {
+  const [pins, setPins] = useState(null);
+  const [pinDetail, setPinDetail] = useState(null);
+  const [comment, setComment] = useState("");
+  const [addingComment, setAddingComment] = useState(false);
+  const { pinId } = useParams();
+
+ 
+
+  const fetchPinDetails = () => {
+    let query = pinDetailQuery(pinId);
+
+    if (query) {
+      client.fetch(query).then((data) => {
+        setPinDetail(data[0]);
+
+        if (data[0]) {
+          query = pinDetailMorePinQuery(data[0]);
+          client.fetch(query).then((res) => {
+            setPins(res);
+          });
+        }
+      });
+    }
+  };
+
+
+  useEffect(() => {
+    fetchPinDetails()
+  }, [pinId])
+  
+
+  if (!pinDetail) return <Spinner message="Loading pin..." />;
+
+  return <div>PinDetail</div>;
 }
