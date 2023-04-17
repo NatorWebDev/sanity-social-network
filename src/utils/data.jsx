2e27ac49af2b61580dfac975eb202273d61288bd
NatorWebDev <126914912+NatorWebDev@users.noreply.github.com>
2023-04-16 20:08:41 -0400

    added create new pin and new data util for categories

--- src/utils/data.jsx
+++ src/utils/data.jsx
@@ -1,3 +1,58 @@
+export const categories = [
+    {
+        name:'cars',
+        image:'https://i.pinimg.com/564x/db/80/d4/db80d46e9384ac61f7c19f01392e1ac6.jpg'
+    },
+    {
+        name:'fitness',
+        image:'https://i.pinimg.com/564x/db/80/d4/db80d46e9384ac61f7c19f01392e1ac6.jpg'
+    },
+    {
+        name:'wallpaper',
+        image:'https://i.pinimg.com/564x/db/80/d4/db80d46e9384ac61f7c19f01392e1ac6.jpg'
+    },
+    {
+        name:'websites',
+        image:'https://i.pinimg.com/564x/db/80/d4/db80d46e9384ac61f7c19f01392e1ac6.jpg'
+    },
+    {
+        name:'photo',
+        image:'https://i.pinimg.com/564x/db/80/d4/db80d46e9384ac61f7c19f01392e1ac6.jpg'
+    },
+    {
+        name:'food',
+        image:'https://i.pinimg.com/564x/db/80/d4/db80d46e9384ac61f7c19f01392e1ac6.jpg'
+    },
+    {
+        name:'nature',
+        image:'https://i.pinimg.com/564x/db/80/d4/db80d46e9384ac61f7c19f01392e1ac6.jpg'
+    },
+    {
+        name:'art',
+        image:'https://i.pinimg.com/564x/db/80/d4/db80d46e9384ac61f7c19f01392e1ac6.jpg'
+    },
+    {
+        name:'travel',
+        image:'https://i.pinimg.com/564x/db/80/d4/db80d46e9384ac61f7c19f01392e1ac6.jpg'
+    },
+    {
+        name:'quotes',
+        image:'https://i.pinimg.com/564x/db/80/d4/db80d46e9384ac61f7c19f01392e1ac6.jpg'
+    },
+    {
+        name:'cats',
+        image:'https://i.pinimg.com/564x/db/80/d4/db80d46e9384ac61f7c19f01392e1ac6.jpg'
+    },
+    {
+        name:'dogs',
+        image:'https://i.pinimg.com/564x/db/80/d4/db80d46e9384ac61f7c19f01392e1ac6.jpg'
+    },
+    {
+        name:'others',
+        image:'https://i.pinimg.com/564x/db/80/d4/db80d46e9384ac61f7c19f01392e1ac6.jpg'
+    },
+]
+
 export const userQuery = (userId)=>{
     const query = `*[_type=='user' && _id=='${userId}']`;
 
